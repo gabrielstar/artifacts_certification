07895d23f98d116a81087c0d4e661627f1f731cb
gabrielstar <gabriel.starczewski@gmail.com>
2022-02-09 13:51:06 +0100

    working on report generation

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,5 @@
  #default docker imahe has no pythin running, you can rebuilt it locally and swap.
- docker build -t jenkins/jenkins:lts .
+ cp ../requirements.txt .
+ docker build --progress=plain -t jenkins/jenkins:lts .
  docker image ls | grep jenkins
+ rm requirements.txt ||:
